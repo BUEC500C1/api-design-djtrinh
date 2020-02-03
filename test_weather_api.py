f14178d88f765433521843d3ebd91e1bc9b0266c
Danny Trinh <djtrinh@bu.edu>
2020-02-03 10:25:14 -0800

    fixed tests to include options

--- test_weather_api.py
+++ test_weather_api.py
@@ -3,19 +3,23 @@ import weather_api
 
 def test_num_input():
     api = weather_api.weather_api()
-    assert api.get_weather("1353") == -1
+    assert api.get_weather("1353", 0) == -1
+    assert api.get_weather("1353", 1) == -1
 
 
 def test_wrong_iata_input():
     api = weather_api.weather_api()
-    assert api.get_weather("wjwkw") == -1
+    assert api.get_weather("wjwkw", 0) == -1
+    assert api.get_weather("wjwkw", 1) == -1
 
 
 def test_nonexistant_iata():
     api = weather_api.weather_api()
-    assert api.get_weather("itv") == -1
+    assert api.get_weather("itv", 0) == -1
+    assert api.get_weather("itv", 1) == -1
 
 
 def test_symbol_input():
     api = weather_api.weather_api()
-    assert api.get_weather("^sj") == -1
+    assert api.get_weather("^sj", 0) == -1
+    assert api.get_weather("^sj", 1) == -1
