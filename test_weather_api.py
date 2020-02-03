48a02563c9a3dc1f1c377e6200e7e9af23e68d50
Danny Trinh <djtrinh@bu.edu>
2020-02-03 09:14:50 -0800

    Removed semicolons

--- test_weather_api.py
+++ test_weather_api.py
@@ -2,20 +2,20 @@ import weather_api
 
 
 def test_num_input():
-    api = weather_api.weather_api();
+    api = weather_api.weather_api()
     assert api.get_weather("1353") == -1
 
 
 def test_wrong_iata_input():
-    api = weather_api.weather_api();
+    api = weather_api.weather_api()
     assert api.get_weather("wjwkw") == -1
 
 
 def test_nonexistant_iata():
-    api = weather_api.weather_api();
+    api = weather_api.weather_api()
     assert api.get_weather("itv") == -1
 
 
 def test_symbol_input():
-    api = weather_api.weather_api();
+    api = weather_api.weather_api()
     assert api.get_weather("^sj") == -1
