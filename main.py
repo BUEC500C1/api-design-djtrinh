daf4a3362617f53c8f811acba7150f20666970ed
Danny Trinh <djtrinh@bu.edu>
2020-02-02 21:18:54 -0800

    Added cli to find and plot temperature within 24-hours

---
+++ main.py
@@ -0,0 +1,58 @@
+import weather_api as w_api
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+
+def plot_data(data, code):
+    periods = data['properties']['periods']
+    
+    x = []
+    x_label = []
+    y = []
+    
+    for i, period in enumerate(periods):
+        if i == 25:
+            break
+        time = int(period['startTime'].split(':')[0][-2:])
+        if time > 12:
+            time = str(time-12)+"pm"
+        elif time == 12:
+            time = "12pm"
+        elif time == 0:
+            time = "12am"
+        else:
+            time = str(time)+"am"
+            
+        x_label.append(time)
+        x.append(i)
+        y.append(period['temperature'])
+
+    sns.set_style("darkgrid")
+    fig, ax = plt.subplots()
+    fig.canvas.draw()
+    plt.plot(x, y)
+    ax.set_xticklabels(x_label)
+    plt.xticks(np.arange(min(x), max(x)+1, 1.0), rotation=45) 
+    plt.ylabel('Temperaute in F')
+    plt.title("Weather Plot for "+code)
+    plt.show()
+            
+
+def main():
+    api = w_api.weather_api()
+
+    while(1):
+        code = input("Enter Airport Code\n")
+        
+        if(code.lower() == "exit"):
+            break
+        else:
+            data = api.get_weather(code)
+            if data != -1:
+                plot_data(data, code)
+            
+
+            
+main()
---
+++ weather_api.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import json
+from urllib.request import urlopen
+
+
+class weather_api():
+
+    
+    def __init__(self):
+        self.airports = pd.read_csv("airports.csv")
+
+    
+    def find_airport_read(self, code):
+        data = self.airports[self.airports['iata_code'].str.contains(code.upper(), na=False)]
+        
+        if(data.empty):
+            print("Please enter a valid airport code\n")
+            return -1
+
+        return data.iloc[0, 4], data.iloc[0, 5]
+
+
+    def get_weather(self, code):
+        if (code.isalpha() == False):
+            print("Please enter a valid airport code\n")
+            return -1
+        elif (len(code) != 3):
+            print("Please enter a valid airport code\n")
+            return -1
+        
+        loc = self.find_airport_read(code)
+
+        if(loc == "error0"):
+            print("Please enter a valid airport code\n")
+            return self.error
+        else:
+            try:
+                response = urlopen('https://api.weather.gov/points/' 
+                                   + str(loc[0])+','+str(loc[1]))
+                data = json.load(response)
+                url = data['properties']['forecastHourly']
+                response = urlopen(url)
+                data = json.load(response)
+                
+                return data
+            except urllib.error.HTTPError:
+                print("Please enter a valid airport code\n")
+                return -1
+                
+            
